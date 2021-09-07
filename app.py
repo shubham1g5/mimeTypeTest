9d0bfdbac8366f009e7a8065af87996300ed167f
Shubham Goyal <shubham1g5@gmail.com>
2021-09-07 17:31:50 +0530

    adds content disposition

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
 @app.route('/page1')
 def page1():
      resp = redirect(url_for('page2'))
-     resp.headers['Content-Type'] = 'application/commcare'
+     resp.headers['content-type'] = 'application/commcare'
      return resp
 
 @app.route('/page2')
 def page2():
-    return 'Hello, World'
+    resp = make_response('Hello, World')
+    resp.headers['content-type'] = 'application/commcare'
+    resp.headers['content-disposition'] = 'inline'
+    return resp
